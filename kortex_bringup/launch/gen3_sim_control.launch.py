286903a474e653c3d363d3895a8240a9368b4d35
Abed <robotogeddon@gmail.com>
2025-06-28 19:33:37 -0400

    Added ros2 simulation control code

---
+++ kortex_bringup/launch/gen3_sim_control.launch.py
@@ -0,0 +1,286 @@
+from launch import LaunchDescription
+from launch.actions import (
+    DeclareLaunchArgument,
+    IncludeLaunchDescription,
+    OpaqueFunction,
+    RegisterEventHandler,
+)
+from launch.event_handlers import OnProcessExit
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.conditions import IfCondition
+from launch.substitutions import (
+    Command,
+    FindExecutable,
+    LaunchConfiguration,
+    PathJoinSubstitution,
+)
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def launch_setup(context, *args, **kwargs):
+    # Initialize Arguments
+    sim_gazebo = LaunchConfiguration("sim_gazebo")
+    robot_type = LaunchConfiguration("robot_type")
+    dof = LaunchConfiguration("dof")
+    # General arguments
+    controllers_file = LaunchConfiguration("controllers_file")
+    description_package = LaunchConfiguration("description_package")
+    description_file = LaunchConfiguration("description_file")
+    robot_name = LaunchConfiguration("robot_name")
+    robot_traj_controller = LaunchConfiguration("robot_controller")
+    robot_hand_controller = LaunchConfiguration("robot_hand_controller")
+    launch_rviz = LaunchConfiguration("launch_rviz")
+    use_sim_time = LaunchConfiguration("use_sim_time")
+    gripper = LaunchConfiguration("gripper")
+
+    robot_controllers = PathJoinSubstitution(
+        [
+            FindPackageShare(description_package),
+            "gen3_arm/config",
+            controllers_file,
+        ]
+    )
+
+    rviz_config_file = PathJoinSubstitution(
+        [FindPackageShare(description_package), "rviz", "view_robot.rviz"]
+    )
+
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [FindPackageShare(description_package), "robots", description_file]
+            ),
+            " ",
+            "robot_ip:=xxx.yyy.zzz.www",
+            " ",
+            "name:=",
+            robot_name,
+            " ",
+            "arm:=",
+            robot_type,
+            " ",
+            "dof:=",
+            dof,
+            " ",
+            "sim_gazebo:=",
+            sim_gazebo,
+            " ",
+            "simulation_controllers:=",
+            robot_controllers,
+            " ",
+            "gripper:=",
+            gripper,
+            " ",
+        ]
+    )
+    robot_description = {"robot_description": robot_description_content.perform(context)}
+
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        parameters=[
+            robot_description,
+            {"use_sim_time": use_sim_time},
+        ],
+    )
+
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=["-d", rviz_config_file],
+        condition=IfCondition(launch_rviz),
+    )
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[
+            "joint_state_broadcaster",
+            "--controller-manager",
+            "/controller_manager",
+        ],
+    )
+
+    # Delay rviz start after `joint_state_broadcaster`
+    delay_rviz_after_joint_state_broadcaster_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=joint_state_broadcaster_spawner,
+            on_exit=[rviz_node],
+        )
+    )
+
+    robot_traj_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[robot_traj_controller, "-c", "/controller_manager"],
+    )
+
+    robot_hand_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[robot_hand_controller, "-c", "/controller_manager"],
+    )
+
+    # Bridge
+    bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=["/clock@rosgraph_msgs/msg/Clock[ignition.msgs.Clock"],
+        output="screen",
+    )
+
+    gazebo_spawn_entity = Node(
+        package="ros_gz_sim",
+        executable="create",
+        output="screen",
+        arguments=[
+            "-string",
+            robot_description_content,
+            "-name",
+            robot_name,
+            "-allow_renaming",
+            "true",
+            "-x",
+            "0.0",
+            "-y",
+            "0.0",
+            "-z",
+            "0.3",
+            "-R",
+            "0.0",
+            "-P",
+            "0.0",
+            "-Y",
+            "0.0",
+        ],
+        condition=IfCondition(sim_gazebo),
+    )
+
+    gazebo_launch_description = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [FindPackageShare("ros_gz_sim"), "/launch/gz_sim.launch.py"]
+        ),
+        launch_arguments={"ign_args": " -r -v 3 empty.sdf"}.items(),
+        condition=IfCondition(sim_gazebo),
+    )
+
+    nodes_to_start = [
+        bridge,
+        robot_state_publisher_node,
+        joint_state_broadcaster_spawner,
+        delay_rviz_after_joint_state_broadcaster_spawner,
+        robot_traj_controller_spawner,
+        robot_hand_controller_spawner,
+        gazebo_launch_description,
+        gazebo_spawn_entity,
+    ]
+
+    return nodes_to_start
+
+
+def generate_launch_description():
+    declared_arguments = []
+    # Simulation specific arguments
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "sim_gazebo",
+            default_value="true",
+            description="Use Gazebo for simulation",
+        )
+    )
+    # Robot specific arguments
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "robot_type",
+            description="Type/series of robot.",
+            choices=["gen3"],
+            default_value="gen3",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "dof",
+            description="DoF of robot.",
+            choices=["7"],
+            default_value="7",
+        )
+    )
+    # General arguments
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "controllers_file",
+            default_value="ros2_controllers.yaml",
+            description="YAML file with the controllers configuration.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "description_package",
+            default_value="kortex_description",
+            description="Description package with robot URDF/XACRO files. Usually the argument \
+        is not set, it enables use of a custom description.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "description_file",
+            default_value="gen3_sim.xacro",
+            description="URDF/XACRO description file with the robot.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "robot_name",
+            default_value="gen3",
+            description="Robot name.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "prefix",
+            default_value='""',
+            description="Prefix of the joint names, useful for \
+        multi-robot setup. If changed than also joint names in the controllers' configuration \
+        have to be updated.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "robot_controller",
+            default_value="joint_trajectory_controller",
+            description="Robot controller to start.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "robot_hand_controller",
+            default_value="robotiq_gripper_controller",
+            description="Robot hand controller to start.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "use_sim_time",
+            default_value="true",
+            description="Use simulated clock",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "gripper",
+            default_value="robotiq_2f_85",
+            choices=["robotiq_2f_85", ""],
+            description="Gripper to use",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument("launch_rviz", default_value="true", description="Launch RViz?")
+    )
+
+    return LaunchDescription(declared_arguments + [OpaqueFunction(function=launch_setup)])
